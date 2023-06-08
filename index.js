b56368f11748c6d9f821d5cf69ca812096a11552
reza107-hub <rashidaminur104@gmail.com>
2023-06-08 22:04:36 +0600

    courses get request by using sort

--- index.js
+++ index.js
@@ -8,8 +8,6 @@ app.use(cors())
 app.use(express.json())
 require('dotenv').config()
 
-
-
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.tfxumrl.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -25,6 +23,15 @@ async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
         // await client.connect();
+
+        const courseCollections = client.db('campSporty').collection('courses')
+
+        app.get('/courses', async (req, res) => {
+            const query = { enrolledStudents: -1}
+            const result = await courseCollections.find().sort(query).toArray()
+            res.send(result)
+        })
+
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
