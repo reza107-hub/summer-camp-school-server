bdb0c292d738a3702e8b331439db298778770779
reza107-hub <rashidaminur104@gmail.com>
2023-06-12 10:25:47 +0600

    admin role done

--- index.js
+++ index.js
@@ -60,6 +60,15 @@ async function run() {
             res.send({ token })
         })
 
+        app.get('/users/admin/:email', async (req, res) => {
+            const email = req.params.email;
+
+            const query = { email: email }
+            const user = await usersCollection.findOne(query);
+            const result = { admin: user?.role === 'admin' }
+            res.send(result);
+        })
+
         app.patch('/users/admin/:id', async (req, res) => {
             const id = req.params.id;
             const filter = { _id: new ObjectId(id) };
